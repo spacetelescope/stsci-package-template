8686b20ea4624c4a02b9fd0c5546e0e458d973b0
Megan Sosey <sosey@stsci.edu>
2018-03-09 11:34:56 -0500

    moved to the stsci_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,12 @@ import importlib
 import sys
 import os
 import sphinx
+import stsci_rtd_theme
+
+def setup(app):
+    app.add_stylesheet("stsci.css")
+
+
 from distutils.version import LooseVersion
 try:
     from ConfigParser import ConfigParser
@@ -80,7 +86,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.autosummary',
     ]
-
 
 if on_rtd:
     extensions.append('sphinx.ext.mathjax')
@@ -195,23 +200,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-html_theme_options = {
-    "collapse_navigation": True
-    }
-#        "nosidebar": "false",
-#        "sidebarbgcolor": "#4db8ff",
-#        "sidebartextcolor": "black",
-#        "sidebarlinkcolor": "black",
-#        "headbgcolor": "white",
-#        }
-
-# Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['_static/']
+html_theme = 'stsci_rtd_theme'
+html_theme_path = [stsci_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -220,25 +210,10 @@ html_theme_path = ['_static/']
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
 
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-html_logo = '_static/stsci_pri_combo_mark_white.png'
-
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 # html_favicon = None
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-html_context = {
-    'css_files': [
-        '_static/css/custom.css',
-    ],
-}
-
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
