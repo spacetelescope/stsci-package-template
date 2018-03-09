6c232b57e5ced6e6c2fda0905514faa626718bb7
Megan Sosey <sosey@stsci.edu>
2018-03-09 11:11:43 -0500

    updated setup for sphinx_build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import os
 import subprocess
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension, Command
 from setuptools.command.test import test as TestCommand
 
 try:
@@ -20,7 +20,8 @@ DESCRIPTION = metadata.get('description', '')
 AUTHOR = metadata.get('author', 'STScI')
 AUTHOR_EMAIL = metadata.get('author_email', 'help@stsci.edu')
 URL = metadata.get('url', 'https://www.stsci.edu/')
-LICENSE = metadata.get('licesnce', 'BSD')
+LICENSE = metadata.get('license', 'BSD')
+
 
 if os.path.exists('relic'):
     sys.path.insert(1, 'relic')
@@ -42,6 +43,44 @@ else:
 version = relic.release.get_info()
 relic.release.write_template(version, PACKAGENAME)
 
+
+# allows you to build sphinx docs from the pacakge
+# main directory with python setup.py build_sphinx
+
+try:
+    from sphinx.cmd.build import build_main
+    from sphinx.setup_command import BuildDoc
+
+    class BuildSphinx(BuildDoc):
+        """Build Sphinx documentation after compiling C source files"""
+
+        description = 'Build Sphinx documentation'
+
+        def initialize_options(self):
+            BuildDoc.initialize_options(self)
+
+        def finalize_options(self):
+            BuildDoc.finalize_options(self)
+
+        def run(self):
+            build_cmd = self.reinitialize_command('build_ext')
+            build_cmd.inplace = 1
+            self.run_command('build_ext')
+            build_main(['-b', 'html', './docs', './docs/_build/html'])
+
+except ImportError:
+    class BuildSphinx(Command):
+        user_options = []
+
+        def initialize_options(self):
+            pass
+
+        def finalize_options(self):
+            pass
+
+        def run(self):
+            print('!\n! Sphinx is not installed!\n!', file=sys.stderr)
+            exit(1)
 
 class PyTest(TestCommand):
     def finalize_options(self):
@@ -78,5 +117,7 @@ setup(
     tests_require=['pytest'],
     packages=find_packages(),
     package_data={PACKAGENAME: ['pars/*']},
-    cmdclass = {'test': PyTest}
-)
+    cmdclass={
+        'test': PyTest,
+        'build_sphinx': BuildSphinx
+    },)
